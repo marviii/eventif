8d0ec2b9ce8e47fa69e58d0c039c8b9a06ca4958
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 20:48:28 +0000

    teste de envio de email

--- contact/tests.py
+++ contact/tests.py
@@ -30,3 +30,23 @@ class ContactPostInvalidTest(TestCase):
         response = self.client.post('/contato/', data=form_data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact_form.html')
+
+class ContactEmailTest(TestCase):
+    def test_contact_email_sent(self):
+        form_data = {
+            'name': 'teste',
+            'phone': '123456789',
+            'email': 'teste@exemplo.com',
+            'message': 'testezinho'
+        }
+
+        response = self.client.post('/contato/', data=form_data)
+        self.assertRedirects(response, '/contato/')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Confirmação de contato')
+
+        self.assertIn('Nome: teste', mail.outbox[0].body)
+        self.assertIn('Telefone: 123456789', mail.outbox[0].body)
+        self.assertIn('Email: teste@exemplo.com', mail.outbox[0].body)
+        self.assertIn('Mensagem: testezinho', mail.outbox[0].body)
