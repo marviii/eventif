486075ce3471077332ff701dbaa388dd6508de3f
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 20:26:30 +0000

    teste 2

--- contact/tests.py
+++ contact/tests.py
@@ -1,10 +1,9 @@
-# contact/tests.py
 from django.test import TestCase
 from django.urls import reverse
-from .views import contact
+from eventif.urls import urlpatterns  # Importe as urlpatterns do arquivo de URLs principal
 
 class ContactGetTest(TestCase):
     def test_contact_form_view(self):
-        response = self.client.get(reverse('contact'))
+        response = self.client.get('/contato/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact_form.html')
