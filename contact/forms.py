e7e4028832d2b26fd24486bce7964640ce53a54a
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 19:51:22 +0000

    tornando telefone não obrigatorio

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Nome')
-    phone = forms.CharField(label='Telefone')
+    phone = forms.CharField(label='Telefone', required=False)
     message = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), label='Mensagem')
     email = forms.EmailField(label='Email')
