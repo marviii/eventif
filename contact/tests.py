69c4fbdf7cb7e32b016997692b06eb36184dd9e9
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 20:31:53 +0000

    teste post valdo e invalido

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,4 @@
+# contact/tests.py
 from django.test import TestCase
 from django.urls import reverse
 from eventif.urls import urlpatterns  # Importe as urlpatterns do arquivo de URLs principal
@@ -5,5 +6,28 @@ from eventif.urls import urlpatterns  # Importe as urlpatterns do arquivo de URL
 class ContactGetTest(TestCase):
     def test_contact_form_view(self):
         response = self.client.get('/contato/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/contact_form.html')
+
+class ContactPostValidTest(TestCase):
+    def test_valid_contact_form_submission(self):
+        form_data = {
+            'name': 'teste',
+            'phone': '123456789',
+            'email': 'teste@exemplo.com',
+            'message': 'testezinho'
+        }
+
+        response = self.client.post(reverse('contact:contact'), data=form_data)
+        self.assertRedirects(response, '/contato/')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Confirmação de contato')
+
+class ContactPostInvalidTest(TestCase):
+    def test_invalid_contact_form_submission(self):
+        form_data = {}  
+
+        response = self.client.post(reverse('contact:contact'), data=form_data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact_form.html')
