4c4fa10a662088ea1b37ea5cf1c48480b42a2294
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 07:20:43 +0000

    criaçao do header contato, views.py e forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label='Nome')
     phone = forms.CharField(label='Telefone')
-    message = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), label='Mensagem')
     email = forms.EmailField(label='Email')
---
+++ contact/views.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect #, HttpResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from contact.forms import ContactForm
+
+def contact(request):
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
+
+def create(request):
+    form = ContactForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, 'contact/contact_form.html', {'form': form})
+
+    _send_mail(
+        'Confirmação de contato',
+        settings.DEFAULT_FROM_EMAIL,
+        form.cleaned_data['email'],
+        'contact/contact_email.txt',
+        form.cleaned_data
+    )
+    messages.success(request, 'Contato realizado com sucesso!')
+    return HttpResponseRedirect('/contato/')
+
+def new(request):
+    return render(request, 'contact/contact_form.html', {'form': ContactForm()})
+
+def _send_mail(subject, from_, to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
