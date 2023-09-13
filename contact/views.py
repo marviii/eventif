54ac95f420993efdba8d3d044437ee7f9a5e422c
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-13 19:46:54 +0000

    refatoração forms e views contact

--- contact/views.py
+++ contact/views.py
@@ -4,33 +4,33 @@ from django.core import mail
 from django.http import HttpResponseRedirect #, HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from contact.forms import ContactForm
+from .forms import ContactForm
 
 def contact(request):
     if request.method == 'POST':
-        return create(request)
+        return create_contact(request)
     else:
-        return new(request)
+        return show_contact_form(request)
 
-def create(request):
+def create_contact(request):
     form = ContactForm(request.POST)
 
     if not form.is_valid():
         return render(request, 'contact/contact_form.html', {'form': form})
 
-    _send_mail(
-        'Confirmação de contato',
-        settings.DEFAULT_FROM_EMAIL,
-        form.cleaned_data['email'],
-        'contact/contact_email.txt',
-        form.cleaned_data
-    )
+    send_confirmation_email(form.cleaned_data)
     messages.success(request, 'Contato realizado com sucesso!')
     return HttpResponseRedirect('/contato/')
 
-def new(request):
+def show_contact_form(request):
     return render(request, 'contact/contact_form.html', {'form': ContactForm()})
 
-def _send_mail(subject, from_, to, template_name, context):
+def send_confirmation_email(data):
+    subject = 'Confirmação de contato'
+    from_email = settings.DEFAULT_FROM_EMAIL
+    to_email = data['email']
+    template_name = 'contact/contact_email.txt'
+    context = data
+
     body = render_to_string(template_name, context)
-    mail.send_mail(subject, body, from_, [from_, to])
+    mail.send_mail(subject, body, from_email, [from_email, to_email])
