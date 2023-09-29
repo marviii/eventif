be357d078e3ebb79903a94100ff99173b6f1ad0b
Marcos Vinicius <84063065+marviii@users.noreply.github.com>
2023-09-29 12:18:42 +0000

    extract urls

--- subscriptions/tests/test_email_subscribe.py
+++ subscriptions/tests/test_email_subscribe.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.core import mail
+from django.shortcuts import resolve_url as r
+
 
 class MailTest(TestCase):
     def setUp(self):
         data = dict(name='Marcos Vinicius', cpf='12345678901', email='marcos.freitas@aluno.riogrande.ifrs.edu.br', phone='53999833674')
-        self.response = self.client.post('/inscricao/', data)
+        self.response = self.client.post(r('subscriptions:new'), data)
         self.email = mail.outbox[0]
 
     def test_subscription_email_subject(self):
